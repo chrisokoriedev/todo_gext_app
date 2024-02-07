84885f9feff72c95ef9f760f07391adea88311f4
christian-iuil <okoriec01@gmail.com>
2024-02-07 13:56:43 +0100

    working on controller

---
+++ lib/app/modules/home/controller/controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:todo_gext_app/app/data/services/storage/respository.dart';
+
+class HomeController extends GetxController {
+  TaskRepository taskRepository;
+  HomeController({required this.taskRepository});
+
+  
+}
