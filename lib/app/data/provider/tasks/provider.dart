3c2e48b691ee2b3c76c45a688da50ac2c41e23e7
christian-iuil <okoriec01@gmail.com>
2024-02-07 11:30:23 +0100

    working on task provider

---
+++ lib/app/data/provider/tasks/provider.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:todo_gext_app/app/core/utlls/keys.dart';
+import 'package:todo_gext_app/app/data/model/task.dart';
+import 'package:todo_gext_app/app/data/services/storage/services.dart';
+
+class TaskProvider {
+  final StorageService _storageService = Get.find<StorageService>();
+  List<TaskModel> readTask() {
+    var tasks = <TaskModel>[];
+    jsonDecode(
+      _storageService.read(taskKey).toString(),
+    ).forEach((e) => tasks.add(TaskModel.fromJson(e)));
+    return tasks;
+  }
+}
