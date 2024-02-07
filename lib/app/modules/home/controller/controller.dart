6575f20e6c1fc0e45553e282da53664b344e86dd
christian-iuil <okoriec01@gmail.com>
2024-02-07 14:37:43 +0100

    worked in cintroller

--- lib/app/modules/home/controller/controller.dart
+++ lib/app/modules/home/controller/controller.dart
@@ -1,9 +1,20 @@
 import 'package:get/get.dart';
+import 'package:todo_gext_app/app/data/model/task.dart';
 import 'package:todo_gext_app/app/data/services/storage/respository.dart';
 
 class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({required this.taskRepository});
-
-  
+  final tasks = <TaskModel>[].obs;
+  @override
+  void onInit() {
+    super.onInit();
+    tasks.assignAll(taskRepository.readTask());
+    ever(tasks, (_) => taskRepository.writeTask(tasks));
+  }
+  @override
+  void onClose() {
+    super.onClose();
+    
+  }
 }
