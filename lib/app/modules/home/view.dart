c00b77701e246a3a4c60f366db3c65e372b0c450
christian-iuil <okoriec01@gmail.com>
2024-02-07 19:38:33 +0100

    added add card to home

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_gext_app/app/modules/home/controller/controller.dart';
 import 'package:todo_gext_app/app/core/utlls/extensions.dart';
+import 'package:todo_gext_app/app/modules/home/widget/add_cart.dart';
 
 class MyHomePage extends GetView<HomeController> {
   const MyHomePage({super.key});
@@ -11,11 +12,17 @@ class MyHomePage extends GetView<HomeController> {
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          padding: EdgeInsets.all(4.0.wp),
+          padding: EdgeInsets.all(4.wp),
           children: [
             Text(
               'My tasks',
-              style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+            ),
+            GridView.count(
+              crossAxisCount: 2,
+              physics: const ClampingScrollPhysics(),
+              shrinkWrap: true,
+              children: [AddCard()],
             ),
           ],
         ),
--- lib/app/modules/home/widget/add_cart.dart
+++ lib/app/modules/home/widget/add_cart.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_gext_app/app/core/utlls/extensions.dart';
@@ -13,8 +14,20 @@ class AddCard extends StatelessWidget {
     final icons = getIcons();
     var squareWidth = Get.width - 12.wp;
     return Container(
-      width: squareWidth/2,
-      height: squareWidth/2,
+      width: squareWidth / 2,
+      height: squareWidth / 2,
+      margin: EdgeInsets.all(3.wp),
+      child: InkWell(
+          onTap: () {},
+          child: DottedBorder(
+              color: Colors.grey[400]!,
+              dashPattern: const [8, 5],
+              child: Center(
+                  child: Icon(
+                Icons.add,
+                size: 10.sp,
+                color: Colors.grey,
+              )))),
     );
   }
 }
