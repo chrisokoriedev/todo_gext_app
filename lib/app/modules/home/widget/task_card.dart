ede3b064cb8dad41e1d895226adb8d31ef9bee74
christian-iuil <okoriec01@gmail.com>
2024-02-17 04:28:23 +0100

    working on task card

---
+++ lib/app/modules/home/widget/task_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_gext_app/app/core/utlls/extensions.dart';
+import 'package:todo_gext_app/app/data/model/task.dart';
+import 'package:todo_gext_app/app/modules/home/controller/controller.dart';
+
+class TaskCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  final TaskModel task;
+  TaskCard({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = HexColor.fromHex(task.color);
+    final squareWidth = Get.width - 12.wp;
+    return  Container(
+      width: squareWidth/2,
+      height: squareWidth/2,
+    );
+  }
+}
