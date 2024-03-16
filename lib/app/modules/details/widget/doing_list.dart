50f379c131177f117ed4f03f8520efb12b354e63
christian-iuil <okoriec01@gmail.com>
2024-03-16 20:09:55 +0100

    update

---
+++ lib/app/modules/details/widget/doing_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_gext_app/app/modules/home/controller/controller.dart';
+
+class DoingList extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  DoingList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
