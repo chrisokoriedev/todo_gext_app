4d47291818bc1da2d8fc613a1f7f347c6a1d9fba
christian-iuil <okoriec01@gmail.com>
2024-03-16 19:26:29 +0100

    update

--- lib/app/modules/details/view.dart
+++ lib/app/modules/details/view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:todo_gext_app/app/core/utlls/extensions.dart';
 import 'package:todo_gext_app/app/modules/home/controller/controller.dart';
 import 'package:todo_gext_app/app/widget/string_const.dart';
@@ -14,39 +16,94 @@ class DetailScreen extends StatelessWidget {
     var color = HexColor.fromHex(task!.color);
     var totalTodos = homeCtrl.doingTask.length + homeCtrl.doneTask.length;
     return Scaffold(
-        body: ListView(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 2.wp, vertical: 3.wp),
-          child: Column(
-            children: [
-              const Align(alignment: Alignment.topLeft, child: BackButton()),
-              SizedBox(height: 2.hp),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4.wp),
-                child: Row(
-                  children: [
-                    Icon(
-                        IconData(task.icon,
-                            fontFamily: AppString.materialIcons),
-                        color: color),
-                    SizedBox(width: 2.wp),
-                    Text(
-                      task.title,
-                      style: TextStyle(
-                          fontSize: 12.sp, fontWeight: FontWeight.w600),
-                    )
-                  ],
+        body: Form(
+      key: homeCtrl.formKey,
+      child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 2.wp, vertical: 3.wp),
+            child: Column(
+              children: [
+                const Align(alignment: Alignment.topLeft, child: BackButton()),
+                SizedBox(height: 2.hp),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.wp),
+                  child: Row(
+                    children: [
+                      Icon(
+                          IconData(task.icon,
+                              fontFamily: AppString.materialIcons),
+                          color: color),
+                      SizedBox(width: 2.wp),
+                      Text(
+                        task.title,
+                        style: TextStyle(
+                            fontSize: 12.sp, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 2.hp),
-              Row(
-                children: [Text('$totalTodos Task')],
-              )
-            ],
+                SizedBox(height: 3.hp),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.wp),
+                  child: Row(
+                    children: [
+                      Text('$totalTodos Task'),
+                      SizedBox(width: 3.wp),
+                      Expanded(
+                          child: StepProgressIndicator(
+                        totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                        currentStep: homeCtrl.doneTask.length,
+                        size: 5,
+                        padding: 0,
+                        selectedGradientColor: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [color.withOpacity(0.5), color]),
+                        unselectedGradientColor: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Colors.grey[300]!,
+                              Colors.grey[300]!,
+                            ]),
+                      ))
+                    ],
+                  ),
+                ),
+                TextFormField(
+                  controller: homeCtrl.editController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[300]!)),
+                    prefixIcon: Icon(Icons.check_box_outline_blank,
+                        color: Colors.grey[400]),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          if (homeCtrl.formKey.currentState!.validate()) {
+                            var success = homeCtrl.addTodo();
+                            if (success) {
+                              EasyLoading.showSuccess(AppString.todoItemAdded);
+                            } else {
+                              EasyLoading.showSuccess(AppString.todoItemExist);
+                            }
+                          }
+                        },
+                        icon: const Icon(Icons.done)),
+                  ),
+                  validator: (text) {
+                    if (text!.trim().isEmpty) {
+                      return AppString.enterTodoItem;
+                    }
+                    return null;
+                  },
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ));
   }
 }
--- lib/app/modules/home/controller/controller.dart
+++ lib/app/modules/home/controller/controller.dart
@@ -89,4 +89,6 @@ class HomeController extends GetxController {
       }
     }
   }
+
+  addTodo() {}
 }
