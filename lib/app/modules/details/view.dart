7aa587df1269bc5dc76e9444984e1ab1497df3ab
christian-iuil <okoriec01@gmail.com>
2024-03-17 18:19:48 +0100

    worked on popscope

--- lib/app/modules/details/view.dart
+++ lib/app/modules/details/view.dart
@@ -31,109 +31,117 @@ class DetailScreen extends StatelessWidget {
 
     var task = homeCtrl.task.value;
     var color = HexColor.fromHex(task!.color);
-    return Scaffold(
-        body: Form(
-      key: homeCtrl.formKey,
-      child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 2.wp, vertical: 3.wp),
-            child: Column(
-              children: [
-                Align(
-                    alignment: Alignment.topLeft,
-                    child: BackButton(
-                      onPressed: () {
-                        Get.back();
-                        homeCtrl.updateTodo();
-                        homeCtrl.changeTask(null);
-                        homeCtrl.editController.clear();
-                      },
-                    )),
-                SizedBox(height: 2.hp),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 4.wp),
-                  child: Row(
-                    children: [
-                      Icon(
-                          IconData(task.icon,
-                              fontFamily: AppString.materialIcons),
-                          color: color),
-                      SizedBox(width: 2.wp),
-                      Text(
-                        task.title,
-                        style: TextStyle(
-                            fontSize: 12.sp, fontWeight: FontWeight.w600),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(height: 3.hp),
-                Obx(() {
-                  var totalTodos =
-                      homeCtrl.doingTodos.length + homeCtrl.doneTodos.length;
-                  return Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.wp),
-                    child: Row(
-                      children: [
-                        Text('$totalTodos Task'),
-                        SizedBox(width: 3.wp),
-                        Expanded(
-                            child: StepProgressIndicator(
-                          totalSteps: totalTodos == 0 ? 1 : totalTodos,
-                          currentStep: homeCtrl.doneTodos.length,
-                          size: 5,
-                          padding: 0,
-                          selectedGradientColor: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [color.withOpacity(0.5), color]),
-                          unselectedGradientColor: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Colors.grey[300]!,
-                                Colors.grey[300]!,
-                              ]),
-                        ))
-                      ],
-                    ),
-                  );
-                }),
-                TextFormField(
-                  controller: homeCtrl.editController,
-                  textInputAction: TextInputAction.done,
-                  autofocus: true,
-                  onFieldSubmitted: (value) {
-                    homeCtrl.editController.clear();
-                    submit();
-                  },
-                  decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey[300]!)),
-                    prefixIcon: Icon(Icons.check_box_outline_blank,
-                        color: Colors.grey[400]),
-                    suffixIcon: IconButton(
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (value) {
+        homeCtrl.updateTodo();
+        homeCtrl.changeTask(null);
+        homeCtrl.editController.clear();
+      },
+      child: Scaffold(
+          body: Form(
+        key: homeCtrl.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 2.wp, vertical: 3.wp),
+              child: Column(
+                children: [
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: BackButton(
                         onPressed: () {
-                          submit();
+                          Get.back();
+                          homeCtrl.updateTodo();
+                          homeCtrl.changeTask(null);
                           homeCtrl.editController.clear();
                         },
-                        icon: const Icon(Icons.done)),
+                      )),
+                  SizedBox(height: 2.hp),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 4.wp),
+                    child: Row(
+                      children: [
+                        Icon(
+                            IconData(task.icon,
+                                fontFamily: AppString.materialIcons),
+                            color: color),
+                        SizedBox(width: 2.wp),
+                        Text(
+                          task.title,
+                          style: TextStyle(
+                              fontSize: 12.sp, fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
                   ),
-                  validator: (text) {
-                    if (text!.trim().isEmpty) {
-                      return AppString.enterTodoItem;
-                    }
-                    return null;
-                  },
-                )
-              ],
+                  SizedBox(height: 3.hp),
+                  Obx(() {
+                    var totalTodos =
+                        homeCtrl.doingTodos.length + homeCtrl.doneTodos.length;
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.wp),
+                      child: Row(
+                        children: [
+                          Text('$totalTodos Task'),
+                          SizedBox(width: 3.wp),
+                          Expanded(
+                              child: StepProgressIndicator(
+                            totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                            currentStep: homeCtrl.doneTodos.length,
+                            size: 5,
+                            padding: 0,
+                            selectedGradientColor: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [color.withOpacity(0.5), color]),
+                            unselectedGradientColor: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Colors.grey[300]!,
+                                  Colors.grey[300]!,
+                                ]),
+                          ))
+                        ],
+                      ),
+                    );
+                  }),
+                  TextFormField(
+                    controller: homeCtrl.editController,
+                    textInputAction: TextInputAction.done,
+                    autofocus: true,
+                    onFieldSubmitted: (value) {
+                      submit();
+                      homeCtrl.editController.clear();
+                    },
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey[300]!)),
+                      prefixIcon: Icon(Icons.check_box_outline_blank,
+                          color: Colors.grey[400]),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            submit();
+                            homeCtrl.editController.clear();
+                          },
+                          icon: const Icon(Icons.done)),
+                    ),
+                    validator: (text) {
+                      if (text!.trim().isEmpty) {
+                        return AppString.enterTodoItem;
+                      }
+                      return null;
+                    },
+                  )
+                ],
+              ),
             ),
-          ),
-          DoingTodoList(),
-          DoneTodoList(),
-        ],
-      ),
-    ));
+            DoingTodoList(),
+            DoneTodoList(),
+          ],
+        ),
+      )),
+    );
   }
 }
