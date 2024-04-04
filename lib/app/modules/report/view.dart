1121a5bb72d9222ae97002ab407e09b06588e141
christian-iuil <okoriec01@gmail.com>
2024-04-04 20:22:50 +0100

    update

--- lib/app/modules/report/view.dart
+++ lib/app/modules/report/view.dart
@@ -40,11 +40,9 @@ class ReportScreen extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      
                       buildStatus(Colors.green, liveTask, 'Live Task'),
                       buildStatus(Colors.orange, completedTask, 'Completed'),
                       buildStatus(Colors.blue, createdTasks, 'Created'),
-
                     ],
                   ),
                   SizedBox(height: 20.wp),
